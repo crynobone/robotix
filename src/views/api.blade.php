0469cc2c3356076439c0e13a32fddfdbf2138037
crynobone <crynobone@gmail.com>
2013-06-18 23:56:56 +0800

    Avoid use of blade {{ }}
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/api.blade.php
+++ src/views/api.blade.php
@@ -2,18 +2,18 @@
 	<h2>Robots.txt</h2>
 </div>
 
-{{ Form::open(array('url' => handles('orchestra/foundation::resources/robotix'), 'method' => 'POST')) }}
+<?php echo Form::open(array('url' => handles('orchestra/foundation::resources/robotix'), 'method' => 'POST')); ?>
 
-	<div class="control-group {{ $errors->has('robots') ? 'has-error' : '' }}">
+	<div class="control-group <?php echo $errors->has('robots') ? 'has-error' : ''; ?>">
 		<div class="controls">
-			{{ Form::textarea('robots', $robots, array('rows' => 10)) }}
-			{{ $errors->first('robots', '<p class="help-block error">:message</p>') }}
+			<?php echo Form::textarea('robots', $robots, array('rows' => 10)); ?>
+			<?php echo $errors->first('robots', '<p class="help-block error">:message</p>'); ?>
 		</div>
 	</div>
 	<div class="form-actions">
 		<button type="submit" class="btn btn-primary">
-			{{ trans('orchestra/foundation::label.submit') }}
+			<?php echo trans('orchestra/foundation::label.submit'); ?>
 		</button>
 	</div>
 
-{{ Form::close() }}
+<?php echo Form::close(); ?>
