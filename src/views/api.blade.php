d399839c31eaaf09ba662dd8e91f456682933984
crynobone <crynobone@gmail.com>
2013-06-20 00:34:35 +0800

    Update Bootstrap 3 CSS.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/api.blade.php
+++ src/views/api.blade.php
@@ -3,17 +3,15 @@
 </div>
 
 <?php echo Form::open(array('url' => handles('orchestra/foundation::resources/robotix'), 'method' => 'POST')); ?>
-
-	<div class="control-group <?php echo $errors->has('robots') ? 'has-error' : ''; ?>">
-		<div class="controls">
+	<fieldset>
+		<div class="<?php echo $errors->has('robots') ? 'has-error' : '' ?>">
 			<?php echo Form::textarea('robots', $robots, array('rows' => 10)); ?>
 			<?php echo $errors->first('robots', '<p class="help-block error">:message</p>'); ?>
+			<p class="help-block"></p>
+			<button type="submit" class="btn btn-primary">
+				<?php echo trans('orchestra/foundation::label.submit'); ?>
+			</button>
 		</div>
-	</div>
-	<div class="form-actions">
-		<button type="submit" class="btn btn-primary">
-			<?php echo trans('orchestra/foundation::label.submit'); ?>
-		</button>
-	</div>
+	</fieldset>
 
 <?php echo Form::close(); ?>
