d5abc0057e995d02915702a97da4deaf63c6168b
crynobone <crynobone@gmail.com>
2013-10-28 00:12:57 +0800

    Implement PSR-2
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Robotix/RobotixServiceProvider.php
+++ src/Robotix/RobotixServiceProvider.php
@@ -2,38 +2,39 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class RobotixServiceProvider extends ServiceProvider {
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
-	/**
+class RobotixServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+    /**
      * Bootstrap the application events.
      *
      * @return void
      */
-	public function boot()
-	{
-		$this->package('crynobone/robotix', 'robotix', __DIR__.'/../');
-		
-		require_once __DIR__.'/../start/global.php';
-		require_once __DIR__.'/../routes.php';
-	}
+    public function boot()
+    {
+        $path = realpath(__DIR__.'/../');
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+        $this->package('crynobone/robotix', 'robotix', $path);
 
+        require_once "{$path}/start/global.php";
+        require_once "{$path}/routes.php";
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -4,10 +4,9 @@ use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 use Orchestra\Support\Facades\App;
 
-Route::any('robots.txt', function ()
-{
-	$robots = App::memory()->get('site.robots-txt', '');
-	$headers['Content-Type'] = 'text/plain; charset=utf-8';
+Route::any('robots.txt', function () {
+    $robots = App::memory()->get('site.robots-txt', '');
+    $headers['Content-Type'] = 'text/plain; charset=utf-8';
 
-	return Response::make($robots, 200, $headers);
+    return Response::make($robots, 200, $headers);
 });
