5321735401986cb8c1543bd85caa3e7a123b4d45
crynobone <crynobone@gmail.com>
2014-01-14 21:46:13 +0800

    Prepare for 2.1.0 release.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/views/api.blade.php
+++ src/views/api.blade.php
@@ -2,16 +2,16 @@
 	<h2>Robots.txt</h2>
 </div>
 
-<?php echo Form::open(array('url' => handles('orchestra/foundation::resources/robotix'), 'method' => 'POST')); ?>
+{{ Form::open(array('url' => handles('orchestra/foundation::resources/robotix'), 'method' => 'POST')) }}
 	<fieldset>
-		<div class="form-group<?php echo $errors->has('robots') ? 'has-error' : '' ?>">
-			<?php echo Form::textarea('robots', $robots, array('rows' => 10, 'class' => 'form-control')); ?>
-			<?php echo $errors->first('robots', '<p class="help-block error">:message</p>'); ?>
+		<div class="form-group{{ $errors->has('robots') ? 'has-error' : '' }}">
+            {{ Form::textarea('robots', $robots, array('rows' => 10, 'class' => 'form-control')) }}
+            {{ $errors->first('robots', '<p class="help-block error">:message</p>') }}
 			<p class="help-block"></p>
 			<button type="submit" class="btn btn-primary">
-				<?php echo trans('orchestra/foundation::label.submit'); ?>
+                {{ trans('orchestra/foundation::label.submit') }}
 			</button>
 		</div>
 	</fieldset>
 
-<?php echo Form::close(); ?>
+{{ Form::close() }}
